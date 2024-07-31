f3d85a435b3026a42ed055bee59a0d9cb4d13bbd
vatsalpanwar <panvatsal@gmail.com>
2024-07-31 18:15:00 +0100

    Making the package installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='crocodel',
+    version='0.0.1',    
+    description='A example Python package',
+    url='https://github.com/vatsalpanwar/crocodel',
+    author='Vatsal Panwar',
+    author_email='panvatsal@gmail.com',
+    license='MIT License',
+    packages=['crocodel'],
+    install_requires=['numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',        
+        'Programming Language :: Python :: 3',
+    ],
+)
